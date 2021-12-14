230f2cb2a63e7e357c148eb91f1b6b63e38f7bdc
Tobias Van Damme <tobias.damme@skf.com>
2021-12-14 23:35:46 +0100

    Solved part b of day 14

--- day14.py
+++ day14.py
@@ -1,5 +1,6 @@
 from aocd import submit, lines
-from collections import Counter
+import collections
+import helper_functions
 
 example_1 = '''NNCB
 
@@ -23,12 +24,19 @@ CN -> C'''
 
 def parse(input):
     template = input[0]
-    insertion_rules = {rule.split(' -> ')[0]: rule.split(' -> ')[1] for rule in input[2:]}
-    return list(template), insertion_rules
+    insertion_rules = {}
+    for rule in input[2:]:
+        pair, new_molecule = rule.split(' -> ')
+        insertion_rules[pair] = [new_molecule, [pair[0] + new_molecule, new_molecule + pair[1]]]
+    # insertion_rules = {rule.split(' -> ')[0]: rule.split(' -> ')[1] for rule in input[2:]}
+    return template, insertion_rules, set([char for pair in insertion_rules.keys() for char in pair])
 
 
-def solve(input, should_submit=False):
-    polymer, insertion_rules = parse(input)
+def generate_full_string(input):
+    polymer, insertion_rules, _ = parse(input)
+    polymer = list(polymer)
+    insertion_rules = {key: val[0] for key, val in insertion_rules.items()}
+    print(''.join(polymer))
     steps = 10
     for _ in range(steps):
         idx = 0
@@ -38,13 +46,44 @@ def solve(input, should_submit=False):
             polymer.insert(idx + 1, new_molecule)
             idx += 2
         print(''.join(polymer))
-        # print(Counter(polymer))
-    counts = Counter(polymer)
-    answer_a = max(counts.values()) - min(counts.values())
-    print(f'The difference between the most occurring and least occurring element is: {answer_a}')
+
+
+def solve(input, should_submit=False):
+    polymer, insertion_rules, unique_letters = parse(input)
+    # print(polymer)
+    # print(insertion_rules)
+    # print(unique_letters)
+    pair_counts = {pair: polymer.count(pair) for pair in insertion_rules.keys()}
+    letter_counts = {letter: polymer.count(letter) for letter in unique_letters}
+    # print(pair_counts)
+    # print(letter_counts)
+    # print()
+    steps = 40
+    for step in range(steps):
+        if step == 10:
+            answer = max(letter_counts.values()) - min(letter_counts.values())
+            print(f'The difference between the most occurring and least occurring element is for part a: {answer}')
+            if should_submit:
+                submit(answer, part='a')
+        new_pair_counts = collections.defaultdict(int)
+        for pair, count in pair_counts.items():
+            if count > 0:
+                # print(pair, count)
+                letter_counts[insertion_rules[pair][0]] += count
+                for new_pair in insertion_rules[pair][1]:
+                    new_pair_counts[new_pair] += count
+        pair_counts = new_pair_counts
+        # print(letter_counts)
+        # print(pair_counts)
+    answer = max(letter_counts.values()) - min(letter_counts.values())
+    print(f'The difference between the most occurring and least occurring element is for part b: {answer}')
     if should_submit:
-        submit(answer_a, part='a')
+        submit(answer, part='b')
 
 
-solve(example_1.splitlines())
+# generate_full_string(example_1.splitlines())
+# generate_full_string(lines)
+# solve(example_1.splitlines())
 solve(lines, True)
+
+# helper_functions.time_function(solve, [lines])
